11f0d80258a7788c25102f5f2868ab84771ffedc
Panu <liimatainenpanu@gmial.com>
2019-11-21 10:49:21 +0200

    dfguh

--- Oppitunti 5/aivot.js
+++ Oppitunti 5/aivot.js
@@ -31,18 +31,61 @@ else if(boxNumber == 3){
 else if (boxNumber == 4){
     document.location.reload();
 }
-    
 }
 function yhteystietoFunktio() {
-    var x = document.getElementById("yhteystietolomake");
-    var text = "";
-    var i;
-    for (i = x.length-1; i >= 0; i--) {
-      text += x.elements[i].name + ": " + x.elements[i].value + "<br>";
-    }
-    document.getElementById("yhteystiedotTuloste").innerHTML = text;
+  var x = document.getElementById("yhteystietolomake");
+  var text = "";
+  var i;
+  for (i = x.length-1; i >= 0; i--) {
+    text += x.elements[i].name + ": " + x.elements[i].value + "<br>";
+  }
+  document.getElementById("yhteystiedotTuloste").innerHTML = text;
+}
+    
+function diagrammiFunktio(){
+  var c = document.getElementById("myCanvas");
+  var arvoArray;
+  var arvoPituus;
+  var piste=40;
+  var diagrammi = c.getContext("2d");
+  arvoArray = [100, 150, 100, 110, 120, 110, 120, 120, 130, 150, +
+      100, 150, 100, 110, 120, 110, 420, 320, 430, 150, +
+      150, 100, 150, 100, 110, 120, 110, 120, 120, 130, 150];
+      
+  arvoPituus= arvoArray.length;
+  
+  diagrammi.beginPath();
+  diagrammi.strokeStyle = "blue";
+
+  diagrammi.moveTo(piste, 500-arvoArray[0]);
+  diagrammi.fillText(arvoArray[0], piste, 500-arvoArray[0]);
+      
+  for (i = 1; i < arvoPituus; i++) {
+      piste=piste+20;
+      diagrammi.lineTo(piste, 500-arvoArray[i]);
+      diagrammi.fillText(arvoArray[i], piste, 500-arvoArray[i]);
+  }
+  diagrammi.stroke();
+
+  diagrammi.fillText("50", 0, 450);
+  diagrammi.fillText("100", 0, 400);
+  diagrammi.fillText("150", 0, 350);
+  diagrammi.fillText("200", 0, 300);
+  diagrammi.fillText("250", 0, 250);
+  diagrammi.fillText("300", 0, 200);
+  diagrammi.fillText("350", 0, 150);
+  diagrammi.fillText("400", 0, 100);
+  diagrammi.fillText("450", 0, 50);
+
+  diagrammi.fillText("5.1.", 120, 500);
+  diagrammi.fillText("10.1.", 220, 500);
+  diagrammi.fillText("15.1.", 320, 500);
+  diagrammi.fillText("20.1.", 420, 500);
+  diagrammi.fillText("25.1.", 520, 500);
+  diagrammi.fillText("30.1.", 620, 500);
 }
 
+/*
 function diagrammiFunktio(){
     var c = document.getElementById ('myCanvas');
     var ctx = c.getContext('2d');
@@ -60,7 +103,7 @@ function diagrammiFunktio(){
       piste=piste+20;
      ctx.fillText(mittariarray[i], piste, arvoarray[i]);    
      }
-/*
+
     ctx.fillText("0", 20, 260);
     ctx.fillText("3", 40, 240);
     ctx.fillText("4", 60, 232);
@@ -75,7 +118,7 @@ function diagrammiFunktio(){
     ctx.fillText("26", 240, 85);
     ctx.fillText("27", 260, 80);
     ctx.fillText("27", 280, 80);
-*/
+
       //viiva
      piste=20;
       ctx.beginPath();
@@ -86,7 +129,10 @@ function diagrammiFunktio(){
        ctx.fillText(arvoarray[i], piste, arvoarray[i]);    
       }
       ctx.stroke();
-      /*
+ 
+}
+
+
       ctx.lineTo(40, 240);
       ctx.lineTo(60, 232);
       ctx.lineTo(80, 227);
@@ -101,7 +147,7 @@ function diagrammiFunktio(){
       ctx.lineTo(260, 80);
       ctx.lineTo(280, 80);
       ctx.stroke();
-*/
+
     //Vaaaka
     ctx.fillText("1", 30, 260);
     ctx.fillText("2", 50, 260);
@@ -132,5 +178,5 @@ function diagrammiFunktio(){
     ctx.fillText("27", 10, 80);
     ctx.fillText("30", 10, 60);
     ctx.fillText("Hevosvoima", 10, 40);
-  
-  }
+*/
+ 
